bc1fe90a848dcc4da1e5b70c4ed460149d615bc3
mkirts <70680675+mkirts@users.noreply.github.com>
2021-04-11 23:38:45 -1000

    Managed to get the profile page to work, but not really

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -13,17 +13,17 @@ class Profile extends React.Component {
     return (this.props.ready) ? this.renderPage() : <Loader active> Getting data, please hold on... </Loader>;
   }
 
-    renderPage() {
-      return (
-          <Container>
-            <Segment>
-              <Item.Group>
-                <Item>
-                  <Item.Image size='medium' src={this.props.userInfo.image}
-                      // eslint-disable-next-line
-                              onError={(i) => i.target.src = '/images/default_image.png'}/>
+  renderPage() {
+    return (
+        <Container>
+          <Segment>
+            <Item.Group>
+              <Item>
+                <Item.Image size='medium' src={this.props.userInfo.image}
+                    // eslint-disable-next-line
+                            onError={(i) => i.target.src = '/images/default_image.png'}/>
                 <Item.Content>
-                  {this.props.currentUser === '' ? (<Button floated ='right'><Icon name='lock'/></Button>) : (
+                  {this.props.currentUser === '' ? (<Button floated='right'><Icon name='lock'/></Button>) : (
                       <Button as={NavLink} exact to={`/EditProfile/${this.props.currentId}`} floated='right'>
                         <Icon name='left chevron'/>Edit Profile
                       </Button>
@@ -32,15 +32,15 @@ class Profile extends React.Component {
                   <Item.Meta>{this.props.userInfo.username}</Item.Meta>
                   <Item.Meta>{this.props.userInfo.number}</Item.Meta>
                   <Item.Description>
-                  <Item.Meta>{this.props.userInfo.description}</Item.Meta>
+                    <Item.Meta>{this.props.userInfo.description}</Item.Meta>
                   </Item.Description>
                 </Item.Content>
-                </Item>
-              </Item.Group>
-            </Segment>
-          </Container>
-      );
-    }
+              </Item>
+            </Item.Group>
+          </Segment>
+        </Container>
+    );
+  }
 }
 
 Profile.propTypes = {
@@ -53,62 +53,11 @@ Profile.propTypes = {
 export default withTracker(({ match }) => {
   const userAccount = Meteor.users.findOne(match.params._id);
   const username = userAccount ? userAccount.username : '';
-  const subscription = Meteor.subscribe('User');
+  const subscription = Meteor.subscribe(User.userPublicationName);
   return {
-    userInfo: User.find({ user: username }) ? User.find({ user: username }) : {},
+    userInfo: User.collection.findOne({ user: username }) ? User.findOne({ user: username }) : {},
     ready: subscription.ready(),
     currentUser: Meteor.user() ? Meteor.user().username : '',
     currentId: match.params._id,
   };
 })(Profile);
-
-    /**
-        <Grid>
-            <Grid.Column width={6}>
-                <Image src='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80'
-     size='medium' circular />
-                <Header as='h1'>Kelsen Reeves</Header>
-                <p>Aloha! I'm Kelsen and I enjoy various technical hobbies when I'm not working.</p>
-                <Button.Group>
-                    <Button>Public Profile</Button>
-                    <Button>Private Profile</Button>
-                </Button.Group>
-            </Grid.Column>
-            <Grid.Column width={3}>
-
-                <Header as='h1'>Top 3 Hobbies</Header>
-                <List ordered>
-                    <List.Item as='a'>HAM Radio</List.Item>
-                    <List.Item as='a'>Japanese Sports Cars</List.Item>
-                    <List.Item as='a'>Cooking</List.Item>
-                </List>
-
-            </Grid.Column>
-            <Grid.Column width={3}>
-                <Header as='h1'>Most Recent Hobbies</Header>
-                <List ordered>
-                    <List.Item as='a'>Crossfit</List.Item>
-                    <List.Item as='a'>HAM Radio</List.Item>
-                    <List.Item as='a'>Reading</List.Item>
-                </List>
-            </Grid.Column>
-        </Grid>
-        // <Segment placeholder>
-        // <Grid columns={2} relaxed='very' stackable>
-        //     <Grid.Column>
-        //         <Image src='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80'
-     size='medium' circular />
-        //         <Header as='h1'>First Header</Header>
-        //         <p>testing 3</p>
-        //     </Grid.Column>
-        //
-        //     <Grid.Column>
-        //         <p>testing right</p>
-        //     </Grid.Column>
-        // </Grid>
-        //     <Divider vertical></Divider>
-        // </Segment>
-    );
-  }
-}
-*/
