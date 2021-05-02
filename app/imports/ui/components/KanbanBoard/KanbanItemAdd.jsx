29aa8e34d4673025cccddebf0fb8064df3cca0e0
angeli-amascual <aamascua@hawaii.edu>
2021-05-01 22:00:14 -1000

    Update dropdown

--- app/imports/ui/components/KanbanBoard/KanbanItemAdd.jsx
+++ app/imports/ui/components/KanbanBoard/KanbanItemAdd.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 import { Grid, Header, Segment } from 'semantic-ui-react';
 import { AutoForm, ErrorsField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
+import { UserHobbies } from '../../../api/user/UserHobbies';
 import { HobbyItems } from '../../../api/HobbyItems/HobbyItems';
-
-/** Create a schema to specify the structure of the data to appear in the form. */
-const formSchema = new SimpleSchema({
-  hobby: String,
-  name: String,
-  status: {
-    type: String,
-    allowedValues: ['Backlog', 'Current', 'Completed'],
-    defaultValue: 'Current',
-  },
-  description: String,
-});
-
-const bridge = new SimpleSchema2Bridge(formSchema);
 
 /** Renders the Page for adding a document. */
 class KanbanItemAdd extends React.Component {
+
+  /** Create a schema to specify the structure of the data to appear in the form. */
+  buildSchema() {
+    const hobbyItems = _.map(this.props.hobbyItems, (item) => item.hobbyName);
+    const formSchema = new SimpleSchema({
+      hobby: {
+        type: String,
+        allowedValues: hobbyItems,
+      },
+      name: String,
+      status: {
+        type: String,
+        allowedValues: ['Backlog', 'Current', 'Completed'],
+        defaultValue: 'Current',
+      },
+      description: String,
+    });
+    return new SimpleSchema2Bridge(formSchema);
+  }
 
   /** On submit, insert the data. */
   submit(data, formRef) {
@@ -42,13 +51,14 @@ class KanbanItemAdd extends React.Component {
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
     let fRef = null;
+    const bridge = this.buildSchema();
     return (
         <Grid container centered>
           <Grid.Column>
             <Header as="h2" textAlign="center">Add Item</Header>
             <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
               <Segment>
-                <TextField name='hobby'/>
+                <SelectField name='hobby'/>
                 <TextField name='name'/>
                 <TextField name='description'/>
                 <SelectField name='status'/>
@@ -62,4 +72,18 @@ class KanbanItemAdd extends React.Component {
   }
 }
 
-export default KanbanItemAdd;
+/** Require an array of Stuff documents in the props. */
+KanbanItemAdd.propTypes = {
+  hobbyItems: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(UserHobbies.userPublicationName);
+  return {
+    hobbyItems: UserHobbies.collection.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(KanbanItemAdd);
